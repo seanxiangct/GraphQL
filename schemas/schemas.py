7786bc632548740465e66b547fd841068c3fad59
Sean Xiang <sean.xiang@sydney.edu.au>
2022-10-03 08:48:42 +1100

    manual ORM to strawberry mapper

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -4,25 +4,50 @@ from strawberry_sqlalchemy_mapper import StrawberrySQLAlchemyMapper
 import models.models as models
 from database.crud import add_person, get_persons
 
-strawberry_sqlalchemy_mapper = StrawberrySQLAlchemyMapper()
-
-
-@strawberry_sqlalchemy_mapper.type(models.Person)
-class Person:
-    pass
-
-
-@strawberry_sqlalchemy_mapper.type(models.Address)
+@strawberry.type
 class Address:
-    pass
+    id: int
+    number: int
+    street: str
+    city: str
+    state: models.StateEnum
 
+    @classmethod
+    def from_model(cls, model):
+        return cls(
+            id=model.id,
+            number=model.number,
+            street=model.street,
+            city=model.city,
+            state=model.state
+        )
+
+@strawberry.type
+class Person:
+    id: strawberry.ID
+    name: str
+    email: str
+    address: list[Address]
+
+    @classmethod
+    def from_model(cls, model):
+        return cls(
+            id=model.id,
+            name=model.name,
+            email=model.email,
+            address=[Address.from_model(address) for address in model.addresses]
+        )
 
 @strawberry.type
 class Query:
     @strawberry.field
     def person(self) -> list[Person]:
         persons = get_persons()
-        return persons
+
+        # convert models to strawberry types
+        
+
+        return [Person.from_model(person) for person in persons]
 
 
 @strawberry.type
@@ -31,6 +56,3 @@ class Mutation:
     def add_person(self, name: str, email: str) -> Person:
         person = add_person(name, email)
         return person
-
-
-strawberry_sqlalchemy_mapper.finalize()
